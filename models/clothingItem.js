41751cddb2b2628ff69649df173f36022a8c50d6
TonyD404 <ajd.davenport@gmail.com>
2026-04-29 23:31:01 -0400

    Project 12 updates with Test corrections

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -10,7 +10,7 @@ const clothingItemSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  imageURL: {
+  imageUrl: {
     type: String,
     required: true,
     validate: {
