4436296cf0399bb3dd105e50e140839b25d298a1
TonyD404 <ajd.davenport@gmail.com>
2026-05-13 00:14:18 -0400

    Additional commit with eslint corrections.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const mainRouter = require("./routes/index");
-//const auth = require("./middlewares/auth");
 const { login, createUser } = require("./controllers/users");
 
 const app = express();
@@ -10,9 +9,7 @@ const { PORT = 3001 } = process.env;
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
-  .then(() => {
-    console.log("Connected to DB");
-  })
+  .then(() => {})
   .catch(console.error);
 
 app.use(express.json());
@@ -20,10 +17,7 @@ app.use(cors());
 
 app.post("/signin", login);
 app.post("/signup", createUser);
-//app.use(auth);
 
 app.use("/", mainRouter);
 
-app.listen(PORT, () => {
-  console.log("Listening on PORT 3001");
-});
+app.listen(PORT, () => {});
