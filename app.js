64cbf3abad441c460f09bcfa4fb06ec2b16cb700
TonyD404 <ajd.davenport@gmail.com>
2026-06-11 14:08:59 -0400

    Updating backend

--- app.js
+++ app.js
@@ -29,6 +29,5 @@ app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`Server is running on http://localhost:${PORT}`);
 });
